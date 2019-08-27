2e2050aac9e9670ddf95c564328ae2d97583cc47
Yintong <zhouyintong96@gmail.com>
2019-08-27 16:36:30 +0200

    update ejs loop

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.get("/fallinlovewith/:thing/", function (req, res) {
 
 app.get("/posts", function (req, res) {
     var posts = [
-        {title: "Post 1", authot: "Susy"},
-        {title: "My adorable pet", authot: "Charlie"},
-        {title: "Can you believe this?", authot: "Colt"}
+        { title: "Post 1", author: "Susy" },
+        { title: "My adorable pet", author: "Charlie" },
+        { title: "Can you believe this?", author: "Colt" }
     ];
-    res.render("posts.ejs", {posts: posts});
+    res.render("posts.ejs", {Posts: posts});
 });
 
-app.get("*", (req, res) => res.send("The page not found!"));
+app.get("*", (req, res) => res.send("The page not found....................."));
 app.listen(3000, () => console.log("Server is started!"));
